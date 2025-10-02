1274a7c0951d2254d95d36555458753f9329efa9
Shantael <agaev.vili@gmail.com>
2025-10-02 13:55:20 +0400

    asd

--- obj/Debug/net9.0/ParsAptekaRu.AssemblyInfo.cs
+++ obj/Debug/net9.0/ParsAptekaRu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParsAptekaRu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac2bba506e9c17d1632193e04cd825ee4be8524")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a469ccfd8276912b46ba2a451585a1ce903fea00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParsAptekaRu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParsAptekaRu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
